45b76d1230e9e50aeb9eb75a49dc6d828778d884
Joe-Chege <njorogejosephchege@gmail.com>
2023-11-03 14:36:10 +0300

    web_dynamic/0-hbnb.py and templates/0-hbnb.html

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -5,20 +5,17 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+import uuid
 
+app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,11 +31,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    cache_id = str(uuid.uuid4())  # Generate a unique cache ID for asset caching
+
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
-
+                           places=places,
+                           cache_id=cache_id)  # Pass cache_id to the template
 
 if __name__ == "__main__":
     """ Main Function """
